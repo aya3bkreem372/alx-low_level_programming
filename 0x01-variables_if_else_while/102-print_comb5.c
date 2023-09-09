b0b492b97b6e760e22f7456a6c44d1838afbc682
aya3bkreem372 <ayaabdelkareem372@gmail.com>
2023-09-09 06:48:59 -0700

    advanced3

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -7,17 +7,19 @@
 int main(void)
 {
 int x, y;
-
 for (x = 0; x <= 99; x++)
 {
 for (y = x; y <= 99; y++)
+{
+if (x < y && x != y)
 {
 putchar(x / 10 + '0');
 putchar(x % 10 + '0');
 putchar(' ');
 putchar(y / 10 + '0');
 putchar(y % 10 + '0');
-if (x != 99 || y != 99)
+}
+else if (x != 98 || y != 99)
 {
 putchar(',');
 putchar(' ');
