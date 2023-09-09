b7c5a35da04c169c14dcf3f40b137f42f2d9d6d1
aya3bkreem372 <ayaabdelkareem372@gmail.com>
2023-09-08 19:52:57 -0700

    numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+* main - is the main function
+*
+*Return: always 0
+*/
+int main(void)
+{
+	int x;
+
+	for (x = 0 ; x < 10 ; x++)
+		putchar(x);
+	putchar('\n');
+	return (0);
+}
