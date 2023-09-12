c32f410fce1fa30389206da65eae3b3a14788126
aya3bkreem372 <ayaabdelkareem372@gmail.com>
2023-09-12 12:15:12 -0700

    task0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,11 +1,20 @@
-#include <unistd.h>
+#include "main.h"
+
 /**
 * main - is the main function
-*Return: always 0
+*
+* Return: always 0
 */
 int main(void)
 {
-char str[] = "_putchar\n";
-write(1, str, sizeof(str) - 1);
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
 return (0);
 }
