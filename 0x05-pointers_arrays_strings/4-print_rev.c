3daab7c6701d4d90588cdcae4c3b80b1013f385c
aya3bkreem372 <ayaabdelkareem372@gmail.com>
2023-09-18 16:10:46 -0700

    task4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+* print_rev - is the function
+* @s: is the parameter
+* Return: void
+*/
+void print_rev(char *s)
+{
+int z;
+int count = 0;
+for (z = 0; s[z] != '\0'; z++)
+{
+count++;
+}
+for (z = count - 1; z <= 0; z--)
+{
+_putchar(s[z]);
+}
+_putchar('\n');
+}
